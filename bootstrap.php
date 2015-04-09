3a3ebb2d9faea180aa8d8744913f8b77362c42e8
Fwolf <fwolf.aide+git@gmail.com>
2015-04-09 09:57:58 +0800

    Use PHPUnit for unit test

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Bootstrap
+ *
+ *  - Register ClassLoader
+ *  - Load default and user config
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+$classLoader = require __DIR__ . '/vendor/autoload.php';
