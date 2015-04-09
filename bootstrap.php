443e3f4d0fb6842b6a28f9c8ce2e62f742018070
Fwolf <fwolf.aide+git@gmail.com>
2015-04-09 14:17:12 +0800

    Load config in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -9,4 +9,16 @@
  * @license     http://opensource.org/licenses/MIT MIT
  */
 
+use Fwlib\Config\GlobalConfig;
+
 $classLoader = require __DIR__ . '/vendor/autoload.php';
+
+
+$configs = require __DIR__ . '/config.default.php';
+if (file_exists(__DIR__ . '/config.php')) {
+    /** @noinspection PhpIncludeInspection */
+    $userConfigs = require __DIR__ . '/config.php';
+    $configs = array_merge($configs, $userConfigs);
+}
+
+GlobalConfig::getInstance()->load($configs);
---
+++ config.default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Default configure file
+ *
+ * For user customized configure, copy this file as 'config.php', and change
+ * value in it, or remove unchanged value. These two files will be automatic
+ * loaded.
+ *
+ * In this file, there can use $userConfig to reference user customized config
+ * value, but remember to give default value if user config not set.
+ *
+ * @copyright   Copyright 2015 Fwolf
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+$config = [];
+
+return $config;
